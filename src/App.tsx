29710e6cf67ca7a9d9461a25746c1bceb022456b
lasting <lastingcoder@qq.com>
2022-04-06 16:58:12 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Shower from './pages/Shower';
 
 function App() {
   return (
     <div className="App">
-      App...
+      <Routes>
+        <Route index element={<Link to='/login'>login</Link>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/shower' element={<Shower />} />
+        <Route path='*' element={<NoMatchPage />}/>
+      </Routes>
     </div>
   );
+}
+
+function NoMatchPage(){
+  return  (
+    <div>
+      <h2>No such Page, please check your url.</h2>
+    </div>
+  )
 }
 
 export default App;
