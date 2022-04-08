9428214746dfa0dd82ae4fe75fde307a60696550
lasting <lastingcoder@qq.com>
2022-04-08 09:25:34 +0800

    ts object type practices

--- src/App.tsx
+++ src/App.tsx
@@ -27,8 +27,10 @@ interface PaintOptions{
 }
 
 function paintShape({shape,xPos=0,yPos=0}:PaintOptions){
-  console.log(shape,xPos,yPos)
+  console.log('pos?0:pos',shape,xPos,yPos)
 }
+
+paintShape({shape:'triggle',xPos:1})
 
 interface ReadOnlyPerson{
   readonly name:string,
@@ -53,11 +55,15 @@ let dog:Dog={
   name: ''
 }
 let cat:Animal=dog
+console.log('dog extend animal',cat)
 
 interface NotOKey{
   [index:string]:Animal,
   [index:number]:Dog
 }
+
+let notok:NotOKey={1:dog}
+console.log('index signatures by string and number',notok)
 
 interface Box{
   name:unknown
@@ -65,9 +71,9 @@ interface Box{
 
 let x:Box={name:'6767'}
 if(typeof x.name === 'string'){
-  console.log(x.name.toLowerCase())
+  console.log('unknow type1',x.name.toLowerCase())
 }
-console.log((x.name as string).toLowerCase())
+console.log('unknow type2',(x.name as string).toLowerCase())
 
 let wPerson:WritablePerson={name:'sdf',age:23}
 let rPerson:ReadOnlyPerson=wPerson
@@ -84,8 +90,29 @@ function getStringArray():StringArray{
 
 let myArray=getStringArray()
 let cc=myArray[1]
-console.log(cc)
+console.log('index signatures',cc)
 
+type StringNumberPair=[string,number]
+
+function doSomething(thing:StringNumberPair):void{
+  thing[0]='213'
+  const [x,y]=thing  
+  console.log('string number pair',x,y)
+}
+
+doSomething(['sdf',234])
+
+type Either2Dor3D=[number,number,number?]
+function print2Dor3D(x:Either2Dor3D){
+  console.log('tuple with question mark change the length',x.length)
+}
+print2Dor3D([1,1])
+print2Dor3D([1,1,1])
+
+type StringNumberBoolean=[string,number,...boolean[]]
+
+const stringnn:StringNumberBoolean=['sdf',234,true,false,true,false]
+console.log('tuple have rest elements',stringnn)
 
 function NoMatchPage(){
   return  (
