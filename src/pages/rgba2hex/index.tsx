746e8b9c38af702f65795a58af55dcd50ee394ec
lasting <lastingcoder@qq.com>
2022-04-09 10:54:44 +0800

    first version

--- src/pages/rgba2hex/index.tsx
+++ src/pages/rgba2hex/index.tsx
@@ -4,28 +4,44 @@ import './index.css'
 
 export default function RgbToHex() {
 
-    const [state, setState] = useState({ R: 255, G: 255, B: 255, A: 100 ,res:'#FFFFFF'})
+    const [state, setState] = useState({ R: 255, G: 255, B: 255, A: 100, res: '#FFFFFF' })
 
-    function myMain(e: any) {
-        let value: string = e.target.value
-        // setState({ ...state, res: hexify(value) })
+
+
+    function changeValue(x: 'R' | 'G' | 'B' | 'A', value: number | null) {
+        let newState = { ...state }
+        newState[x] = value == null ? 0 : value
+
+        let a = newState.A * 0.01,
+            r = Math.floor(a * newState.R + (1 - a) * 255),
+            g = Math.floor(a * newState.G + (1 - a) * 255),
+            b = Math.floor(a * newState.B + (1 - a) * 255);
+
+        newState['res'] = "#" +
+            ("0" + r.toString(16)).slice(-2) +
+            ("0" + g.toString(16)).slice(-2) +
+            ("0" + b.toString(16)).slice(-2)
+
+        setState(newState)
     }
 
     return (
         <div className="rgba2Hex">
             <div>
-                R=<InputNumber min={0} max={255} value={state.R} />
-                G=<InputNumber min={0} max={255} defaultValue={state.G} />
-                B=<InputNumber min={0} max={255} defaultValue={state.B} />
+                R=<InputNumber min={0} max={255} value={state.R} onChange={(value: number | null) => changeValue('R', value)} />
+                G=<InputNumber min={0} max={255} defaultValue={state.G} onChange={(value: number | null) => changeValue('G', value)} />
+                B=<InputNumber min={0} max={255} defaultValue={state.B} onChange={(value: number | null) => changeValue('B', value)} />
                 A=<InputNumber
                     defaultValue={state.A}
                     min={0}
                     max={100}
-                    formatter={value => `${value}%`}
+                    style={{width:'110px'}}
+                    addonAfter='%'
+                    onChange={(value: number | null) => changeValue('A', value)}
                 />
             </div>
-            <div>
-                result:<span className="result">{state.res}</span>
+            <div className="result">
+                result:{state.res}
             </div>
         </div>
     )
@@ -47,7 +63,3 @@ function hexify(color: string) {
         ("0" + g.toString(16)).slice(-2) +
         ("0" + b.toString(16)).slice(-2);
 }
-
-var myHex = hexify('rgba(13, 101, 80, 0.495)'); // "#f5faf3"
-
-console.log(myHex);
