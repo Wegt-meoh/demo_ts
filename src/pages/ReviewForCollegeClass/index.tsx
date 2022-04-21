0928b7664b069e9b796ad3a07b8a614de156180d
lasting <lastingcoder@qq.com>
2022-04-21 19:56:39 +0800

    add more content

--- src/pages/ReviewForCollegeClass/index.tsx
+++ src/pages/ReviewForCollegeClass/index.tsx
@@ -16,10 +16,10 @@ export default function ReviewForCollegeClass() {
             first H2
             <H3 title='a标签'>
               <Code>
-                  let a=1&
-                  console.log(a)&
-                  ++a=sdfsfd&
-                  a.foreach
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach
               </Code>
             </H3>
           </H2>
@@ -35,6 +35,142 @@ export default function ReviewForCollegeClass() {
         <H1 title='h1标签' />
         <H2 title='html'>
           <Code>asfasfds</Code>
+        </H2>
+        <H1 title='h1'>
+          h1标签的简介
+          <H2 title='a标签'>
+            first H2
+            <H3 title='a标签'>
+              <Code>
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach
+              </Code>
+            </H3>
+          </H2>
+          <H2 title='bbb'>
+            Second H2
+          </H2>
+          <H2 title='vcv'>
+            Third H3
+            <>sdaf</>
+            <>sdfasd</>
+          </H2>
+        </H1>
+        <H1 title='h1标签' />
+        <H2 title='html'>
+          <Code>asfasfds</Code>
+        </H2>
+        <H1 title='h1'>
+          h1标签的简介
+          <H2 title='a标签'>
+            first H2
+            <H3 title='a标签'>
+              <Code>
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+              </Code>
+            </H3>
+          </H2>
+          <H2 title='bbb'>
+            Second H2
+          </H2>
+          <H2 title='vcv'>
+            Third H3
+            <>sdaf</>
+            <>sdfasd</>
+          </H2>
+        </H1>
+        <H1 title='h1标签' />
+        <H2 title='html'>
+          <Code>asfasfds</Code>
+        </H2>
+        <H1 title='h1'>
+          h1标签的简介
+          <H2 title='a标签'>
+            first H2
+            <H3 title='a标签'>
+              <Code>
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+                let a=1&
+                console.log(a)&
+                ++a=sdfsfd&
+                a.foreach&
+              </Code>
+            </H3>
+          </H2>
+          <H2 title='bbb'>
+            Second H2
+          </H2>
+          <H2 title='vcv'>
+            Third H3
+            <>sdaf</>
+            <>sdfasd</>
+          </H2>
+        </H1>
+        <H1 title='h1标签' />
+        <H2 title='html'>
+          <Code>
+            asfasfds&
+            let a=1&
+            console.log(a)&
+            ++a=sdfsfd&
+            a.foreach&
+            let a=1&
+            console.log(a)&
+            ++a=sdfsfd&
+            a.foreach&
+            let a=1&
+            console.log(a)&
+            ++a=sdfsfd&
+            a.foreach&
+            let a=1&
+            console.log(a)&
+            ++a=sdfsfd&
+            a.foreach&
+          </Code>
         </H2>
       </>
     </DocsifyContainer >
