fa176b06f07ed9e07bd6380435d6a25592d22dc0
lasting <lastingcoder@qq.com>
2022-04-25 14:18:57 +0800

    more test content

--- src/pages/ReviewForCollegeClass/index.tsx
+++ src/pages/ReviewForCollegeClass/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Code, DocsifyContainer, H1, H2, H3 } from '../../components/DocsifyComponents'
+import { Code, DocsifyContainer, Header } from '../../components/DocsifyComponents'
 
 
 import './index.css'
@@ -8,119 +8,119 @@ import './index.css'
 export default function ReviewForCollegeClass() {
 
   return (
-    <DocsifyContainer>
+    <DocsifyContainer minContentWidth={900}>
       <>
-        <H1 title='h1'>
-          <H2 title='h2'>
-            <H3 title='h3'>
-              <H3 title='h4'>
-                <H3 title='h5'>
+        <Header title='sdf'>
+          sadf
+        </Header>
+        <Code>
+          新的操作系统：&
+          --命名为开源欧拉&
+          ----满足  服务器，个人开发，边缘计算等需求&
+          ----for(let i=0;i&lt;a;i++)
+        </Code>
+        <a href='https://www.baidu.com'>baidu</a>
+        <Header title='h1'>
+          <Header title='h2'>
+            <Header title='h3'>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-              <H3 title='这是一个很长的h4标签的这是一个很长的h4标签的这是一个很长的h4标签的这是一个很长的h4标签的'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+              <Header title='这是一个很长的h4标签的这是一个很长的h4标签的这是一个很长的h4标签的这是一个很长的h4标签的'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-            </H3>
-            <H3 title='h3'>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+            </Header>
+            <Header title='h3'>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-            </H3>
-          </H2>
-          <H2 title='h2'>
-            <H3 title='h3'>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+            </Header>
+          </Header>
+          <Header title='h2'>
+            <Header title='h3'>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-            </H3>
-            <H3 title='h3'>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+            </Header>
+            <Header title='h3'>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-              <H3 title='h4'>
-                <H3 title='h5'>
+                </Header>
+              </Header>
+              <Header title='h4'>
+                <Header title='h5'>
 
-                </H3>
-              </H3>
-            </H3>
-          </H2>
-        </H1>
-        <H1 title='h1'>
-          h1标签的简介
-          <H2 title='a标签'>
-            first H2
-            <H3 title='a标签'>
+                </Header>
+              </Header>
+            </Header>
+          </Header>
+        </Header>
+        <Header title='h1'>
+          <Header title='a标签'>
+            <Header title='a标签'>
+              <Code>
+                let a=1&
+                --console.log(a)&
+                --a=sdfsfd&
+                a.foreach
+              </Code>
+            </Header>
+          </Header>
+          <Header title='bbb'>
+          </Header>
+          <Header title='vcv'>
+            <>sdaf</>
+            <>sdfasd</>
+          </Header>
+        </Header>
+        <Header title='h1标签' />
+        <Header title='html'>
+          <Code>asfasfds</Code>
+        </Header>
+        <Header title='h1'>
+          <Header title='a标签'>
+            <Header title='a标签'>
               <Code>
                 let a=1&
                 console.log(a)&
                 ++a=sdfsfd&
                 a.foreach
               </Code>
-            </H3>
-          </H2>
-          <H2 title='bbb'>
-            Second H2
-          </H2>
-          <H2 title='vcv'>
-            Third H3
+            </Header>
+          </Header>
+          <Header title='bbb'>
+          </Header>
+          <Header title='vcv'>
             <>sdaf</>
             <>sdfasd</>
-          </H2>
-        </H1>
-        <H1 title='h1标签' />
-        <H2 title='html'>
+          </Header>
+        </Header>
+        <Header title='h1标签' />
+        <Header title='html'>
           <Code>asfasfds</Code>
-        </H2>
-        <H1 title='h1'>
-          h1标签的简介
-          <H2 title='a标签'>
-            first H2
-            <H3 title='a标签'>
-              <Code>
-                let a=1&
-                console.log(a)&
-                ++a=sdfsfd&
-                a.foreach
-              </Code>
-            </H3>
-          </H2>
-          <H2 title='bbb'>
-            Second H2
-          </H2>
-          <H2 title='vcv'>
-            Third H3
-            <>sdaf</>
-            <>sdfasd</>
-          </H2>
-        </H1>
-        <H1 title='h1标签' />
-        <H2 title='html'>
-          <Code>asfasfds</Code>
-        </H2>
-        <H1 title='h1'>
-          h1标签的简介
-          <H2 title='a标签'>
-            first H2
-            <H3 title='a标签'>
+        </Header>
+        <Header title='h1'>
+          <Header title='a标签'>
+            <Header title='a标签'>
               <Code>
                 let a=1&
                 console.log(a)&
@@ -147,75 +147,71 @@ export default function ReviewForCollegeClass() {
                 ++a=sdfsfd&
                 a.foreach&
               </Code>
-            </H3>
-          </H2>
-          <H2 title='bbb'>
-            Second H2
-          </H2>
-          <H2 title='vcv'>
-            Third H3
+            </Header>
+          </Header>
+          <Header title='bbb'>
+          </Header>
+          <Header title='vcv'>
             <>sdaf</>
             <>sdfasd</>
-          </H2>
-        </H1>
-        <H1 title='h1标签' />
-        <H2 title='html'>
+          </Header>
+        </Header>
+        <Header title='h1标签' />
+        <Header title='html'>
           <Code>asfasfds</Code>
-        </H2>
-        <H1 title='h1'>
-          h1标签的简介
-          <H1 title='masldfldjny'>
-            <H1 title='mandfksdjfy'>
-              <H1 title='mandlkfjsdlky'>
-                <H1 title='masldfldjny'>
-                  <H1 title='mandfksdjfy'>
-                    <H1 title='mandlkfjsdlky'>
-                      <H1 title='masldfldjny'>
-                        <H1 title='mandfksdjfy'>
-                          <H1 title='mandlkfjsdlky'>
-                            <H1 title='masldfldjny'>
-                              <H1 title='mandfksdjfy'>
-                                <H1 title='mandlkfjsdlky'>
-                                  <H1 title='masldfldjny'>
-                                    <H1 title='mandfksdjfy'>
-                                      <H1 title='mandlkfjsdlky'>
-                                        <H1 title='masldfldjny'>
-                                          <H1 title='mandfksdjfy'>
-                                            <H1 title='mandlkfjsdlky'>
-                                              <H1 title='masldfldjny'>
-                                                <H1 title='mandfksdjfy'>
-                                                  <H1 title='mandlkfjsdlky'>
-                                                    <H1 title='masldfldjny'>
-                                                      <H1 title='mandfksdjfy'>
-                                                        <H1 title='mandlkfjsdlky'>
+        </Header>
+        <Header title='h1'>
+          <Header title='masldfldjny'>
+            <Header title='mandfksdjfy'>
+              <Header title='mandlkfjsdlky'>
+                <Header title='masldfldjny'>
+                  <Header title='mandfksdjfy'>
+                    <Header title='mandlkfjsdlky'>
+                      <Header title='masldfldjny'>
+                        <Header title='mandfksdjfy'>
+                          <Header title='mandlkfjsdlky'>
+                            <Header title='masldfldjny'>
+                              <Header title='mandfksdjfy'>
+                                <Header title='mandlkfjsdlky'>
+                                  <Header title='masldfldjny'>
+                                    <Header title='mandfksdjfy'>
+                                      <Header title='mandlkfjsdlky'>
+                                        <Header title='masldfldjny'>
+                                          <Header title='mandfksdjfy'>
+                                            <Header title='mandlkfjsdlky'>
+                                              <Header title='masldfldjny'>
+                                                <Header title='mandfksdjfy'>
+                                                  <Header title='mandlkfjsdlky'>
+                                                    <Header title='masldfldjny'>
+                                                      <Header title='mandfksdjfy'>
+                                                        <Header title='mandlkfjsdlky'>
 
-                                                        </H1>
-                                                      </H1>
-                                                    </H1>
-                                                  </H1>
-                                                </H1>
-                                              </H1>
-                                            </H1>
-                                          </H1>
-                                        </H1>
-                                      </H1>
-                                    </H1>
-                                  </H1>
-                                </H1>
-                              </H1>
-                            </H1>
-                          </H1>
-                        </H1>
-                      </H1>
-                    </H1>
-                  </H1>
-                </H1>
-              </H1>
-            </H1>
-          </H1>
-          <H2 title='a标签'>
-            first H2
-            <H3 title='a标签'>
+                                                        </Header>
+                                                      </Header>
+                                                    </Header>
+                                                  </Header>
+                                                </Header>
+                                              </Header>
+                                            </Header>
+                                          </Header>
+                                        </Header>
+                                      </Header>
+                                    </Header>
+                                  </Header>
+                                </Header>
+                              </Header>
+                            </Header>
+                          </Header>
+                        </Header>
+                      </Header>
+                    </Header>
+                  </Header>
+                </Header>
+              </Header>
+            </Header>
+          </Header>
+          <Header title='a标签'>
+            <Header title='a标签'>
               <Code>
                 let a=1&
                 console.log(a)&
@@ -242,19 +238,17 @@ export default function ReviewForCollegeClass() {
                 ++a=sdfsfd&
                 a.foreach&
               </Code>
-            </H3>
-          </H2>
-          <H2 title='bbb'>
-            Second H2
-          </H2>
-          <H2 title='vcv'>
-            Third H3
+            </Header>
+          </Header>
+          <Header title='bbb'>
+          </Header>
+          <Header title='vcv'>
             <>sdaf</>
             <>sdfasd</>
-          </H2>
-        </H1>
-        <H1 title='h1标签' />
-        <H2 title='html'>
+          </Header>
+        </Header>
+        <Header title='h1标签' />
+        <Header title='html'>
           <Code>
             asfasfds&
             let a=1&
@@ -274,7 +268,7 @@ export default function ReviewForCollegeClass() {
             ++a=sdfsfd&
             a.foreach&
           </Code>
-        </H2>
+        </Header>
       </>
     </DocsifyContainer >
   )
