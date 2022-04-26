74d3856adc04b964f2889e143f091682959876b6
lasting <lastingcoder@qq.com>
2022-04-26 09:10:42 +0800

    * a with href * img with src and alt * img can not show expected src

--- src/pages/ReviewForCollegeClass/index.tsx
+++ src/pages/ReviewForCollegeClass/index.tsx
@@ -8,21 +8,23 @@ import './index.css'
 export default function ReviewForCollegeClass() {
 
   return (
-    <DocsifyContainer minContentWidth={900}>
+    <DocsifyContainer minContentWidth={900} subMaxLevel={4}>
       <>
         <Header title='sdf'>
           sadf
+          <a href='http://www.baidu.com'>baidu</a>
+          <img src="./images/001.png" width='400px' alt="loading..." />
         </Header>
         <Code>
           新的操作系统：&
           --命名为开源欧拉&
-          ----满足  服务器，个人开发，边缘计算等需求&
+          ----满足            服务器，个人开发，边缘计算等需求&
           ----for(let i=0;i&lt;a;i++)
         </Code>
         <a href='https://www.baidu.com'>baidu</a>
         <Header title='h1'>
-          <Header title='h2'>
-            <Header title='h3'>
+          <Header title='h2' size='h3'>
+            <Header title='h3' size='h3'>
               <Header title='h4'>
                 <Header title='h5'>
 
