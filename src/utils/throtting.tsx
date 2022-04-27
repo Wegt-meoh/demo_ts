d3985216c6782af1b92b148066a1fb6db79f38e0
lasting <lastingcoder@qq.com>
2022-04-27 14:20:01 +0800

    throtting

---
+++ src/utils/throtting.tsx
@@ -0,0 +1,14 @@
+export default function throtting(func: (args?: any) => any, delay: number, maxWaitTime: number) {
+    let timer: NodeJS.Timeout | null = null
+    let startTime = Date.now()
+    return function () {
+        if (timer !== null) clearTimeout(timer)
+        const waitTime = Date.now() - startTime
+        if (waitTime >= maxWaitTime) {
+            func()
+            startTime += waitTime
+        } else {
+            timer = setTimeout(func, delay)
+        }
+    }
+}
