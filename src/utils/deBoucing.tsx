70998f127e066a0a583a0b45cc1159526c58892f
lasting <lastingcoder@qq.com>
2022-04-27 14:19:52 +0800

    debouncing

---
+++ src/utils/deBoucing.tsx
@@ -0,0 +1,7 @@
+export default function deBouncing(func: (args?: any) => any, delay: number) {
+    let timer: NodeJS.Timeout | null = null
+    return function () {
+        if (timer !== null) clearTimeout(timer)
+        timer = setTimeout(func, delay)
+    }
+}
