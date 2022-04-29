273096a7345e94584dcdea5dc174587d1ec347a5
lasting <lastingcoder@qq.com>
2022-04-29 20:40:32 +0800

    dev..

--- src/index.tsx
+++ src/index.tsx
@@ -5,5 +5,5 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 //in ts strict mode it can not be work
-const root=ReactDOM.createRoot(document.getElementById('root'))
+const root=ReactDOM.createRoot(document.getElementById('root') as unknown as Element)
 root.render(<BrowserRouter><App/></BrowserRouter>)
