a1843e7749024e912c3bc47fe8464b19a8e690ae
lasting <lastingcoder@qq.com>
2022-05-02 19:56:43 +0800

    register in DocsifyContainer for sync

--- src/components/DocsifyComponents/DocsifyHeaderLink.tsx
+++ src/components/DocsifyComponents/DocsifyHeaderLink.tsx
@@ -2,40 +2,27 @@ import { useEffect, useState } from "react"
 import { HeaderProps } from "./Header"
 
 interface DocsifyHeaderLinkProps extends HeaderProps {
-    register: (x: string) => boolean
+    headerId:string    
+    href:string
 }
 
-export default function DocsifyHeaderLink(props: DocsifyHeaderLinkProps) {
+const sizeStyle = { h3: 'Docsify-content-artical-h3', h2: 'Docsify-content-artical-h2', h1: 'Docsify-content-artical-h1' }
 
+export default function DocsifyHeaderLink(props: DocsifyHeaderLinkProps) {
     const {
         size = 'h1',
         title,
+        headerId,
         children,
-        register,
+        href
     } = props
 
-    const [headerId, setHeaderId] = useState<string>(title)
-    const [hrefHash, setHrefHash] = useState<string>('#/?id=' + encodeURI(title))
-    const sizeStyle = { h3: 'Docsify-content-artical-h3', h2: 'Docsify-content-artical-h2', h1: 'Docsify-content-artical-h1' }
-
-    // By default, effects run after every completed render,
-    // but you can choose to fire them only when certain values have changed.
-    // If you want to run an effect and clean it up only once (on mount and unmount),
-    // you can pass an empty array ([]) as a second argument.
-    // register href here
-    useEffect(() => {
-        if (register(headerId) === false) {
-            let index = 1
-            while (register(headerId + '-' + index) === false) index++
-            //then you need rerender the component
-            setHeaderId(headerId + '-' + index)
-            setHrefHash('#/?id=' + encodeURI(headerId + '-' + index))
-        }
-    }, [])
+    
+    
     return (
         <>
             <h1 id={headerId} className={sizeStyle[size]}>
-                <a href={hrefHash}>
+                <a href={href}>
                     {title}
                 </a>
             </h1>
